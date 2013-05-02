4bd3c5f8e5336f2453c007ab46fdf8a07837bd66
Killua <killua_hzl@163.com>
2013-05-02 21:11:37 +0800

    commit level0, level1, level2 and level3

---
+++ level00.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python
+#coding:utf8
+'''
+Created on 2012-3-15
+
+@author: killua
+@url: http://www.pythonchallenge.com/pc/def/0.html
+@target: http://www.pythonchallenge.com/pc/def/274877906944.html
+
+Hint:
+    计算2的38次方
+'''
+
+print 2 ** 38
+
+
---
+++ level01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env/python
+#coding:utf8
+'''
+Created on 2012-3-15
+
+@author: killua
+@url:http://www.pythonchallenge.com/pc/def/map.html
+@target: @url:http://www.pythonchallenge.com/pc/def/ocr.html
+
+Hint:
+    K + 2 = M
+    O + 2 = Q
+    E + 2 = G
+    也就是译码问题，将所有字母的ASC码加2
+'''
+
+#version 1
+print chr(ord("m") + 2)+chr(ord("a") + 2)+chr(ord("p") + 2)
+
+"""
+PS:
+字符与ASCII转换
+char->ascii ord()
+ascii->char chr()
+"""
+
+#version 2
+print '*' * 20
+
+import string 
+
+s1 = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+s2 = "map"
+t = t=string.maketrans( 'abcdefghijklmnopqrstuvwxyz ', 'cdefghijklmnopqrstuvwxyzab ')
+print string.translate(s1,t)
+print string.translate(s2, t)
